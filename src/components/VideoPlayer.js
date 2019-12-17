7a71d1bbdbd55f9bd8cd7726105fd3ff5295f8e6
Therese Brown <browntm96@gmail.com>
2019-12-17 15:47:33 -0500

    Work in progress

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -7,6 +7,8 @@ var VideoPlayer = ({video}) => (
       <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${video.id.videoId}?autoplay=true`} allowFullScreen></iframe>
       </div>
+      {/* VideoDetails component here?
+      with below making up the video details*/}
       <div className="video-player-details">
         <h3>{video.snippet.title}</h3>
         <div>{video.snippet.description}</div>
